d34c2a34c3049b6e4c773b7630b7534acf8a1d67
byungshik <byungshik@172.21.21.17>
2015-07-01 17:25:36 +0900

    Hello World.!!

---
+++ pilotproject/src/main/java/com/daou/daouqa/pilot/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.daou.daouqa.pilot;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ pilotproject/src/main/java/com/daou/daouqa/pilot/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author byungshik
+ *
+ */
+package com.daou.daouqa.pilot;
